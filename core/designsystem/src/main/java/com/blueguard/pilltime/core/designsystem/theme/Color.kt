79cc55dfd8c3ae35d304f909946605f0dfa8148a
김경호 <kkh9331@naver.com>
2023-12-18 00:02:54 +0900

    [init] multimodule 초기세팅 완료

---
+++ core/designsystem/src/main/java/com/blueguard/pilltime/core/designsystem/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.blueguard.pilltime.core.designsystem.theme
+
+import androidx.compose.ui.graphics.Color
+
+internal val pink = Color(0xFFFF5C74)
+internal val green = Color(0xFF47EC98)
+internal val blue = Color(0xFF0EE2FF)
+internal val red = Color(0xFFFF3532)
+internal val gray10 = Color(0xFF3E454D)
+internal val gray20 = Color(0xFF2E3339)
+internal val blueBlack = Color(0xFF000A13)
