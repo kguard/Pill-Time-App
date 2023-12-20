f4607840b96fe7a10339c24c89a6768336c1b1f3
pureum <lpm0831@naver.com>
2023-12-20 18:27:37 +0900

    [add] splash screen fade out 기능 추가

--- app/src/main/java/com/blueguard/pilltime/MainActivity.kt
+++ app/src/main/java/com/blueguard/pilltime/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.blueguard.pilltime
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnticipateInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.animation.doOnEnd
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
@@ -17,21 +21,27 @@ class MainActivity : ComponentActivity() {
         var gap = false
 
         val splashScreen = installSplashScreen()
-
         super.onCreate(savedInstanceState)
+
+        splashScreen.setOnExitAnimationListener { splashScreenProvider ->
+            val fadeOut = ObjectAnimator.ofFloat(splashScreenProvider.view, View.ALPHA, 0f)
+            fadeOut.duration = 250L
+            fadeOut.doOnEnd { splashScreenProvider.remove() }
+            fadeOut.start()
+        }
+
 
         lifecycleScope.launch {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
                 //여기서 원하는 작업할때까지 대기할 수 있으며,
-                delay(1500L)
+                delay(1000L)
                 gap = true
             }
         }
 
         splashScreen.setKeepOnScreenCondition{
             // gap을 옵져버하고있기 때문에 변화를 감지해서 넘어갈 수 있음
-            if(gap) false
-            else true
+            !gap
         }
 
         setContent {
